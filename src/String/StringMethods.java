bee5bff443228f1583d7c74f55c110a44a43b139
Navnath0006 <navnathkamble0007@gmail.com>
2023-04-11 11:17:44 +0530

    after configure

--- src/String/StringMethods.java
+++ src/String/StringMethods.java
@@ -9,10 +9,19 @@ public class StringMethods {
 		System.out.println(s1.isBlank());
 		System.out.println(s1.substring(4, 6));
 		
+		String s3 = "New Setup for Jenkins";
+		
+		String[] jenkins = s3.split(" ");
+		for(int i=0;i<=jenkins.length-1;i++) {
+			System.out.println(jenkins[i]);
+		}
+		
+		
 		String s2 = "My Name is Navnath";
 		String[] ar = s2.split(" ");
 		for(String data:ar) {
 			System.out.println(data.trim());
 		}
+		
 	}
 }
